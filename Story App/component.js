4daf7ae7d09a53d307312fd3861a36cb5cbf31c0
Brando Preda <brando.preda@trovaprezzi.it>
2017-09-28 17:51:06 +0200

    started new comments app using bootstrap

--- Story App/component.js
+++ Story App/component.js
@@ -1,23 +1,40 @@
-class StoryBox extends React.Component {
+class CommentBox extends React.Component {
 	render(){
-		const topics = ['HTML', 'JavaScript', 'React'];
-		const now = new Date();
-		
-		return ( 
-			<div>
-				<h3>Stories</h3>
-				<p className="lead">
-					Current time: {now.toTimeString()}
-				</p>
-				<ul>
-					{topics.map( topic => <li>{topic}</li>)}
-				</ul>
-			</div> 
+		return(
+			<div className="container">
+				<h1>Comment Box App</h1>
+				<div className="comment-box">
+					<h4 className="comment-count text-secondary">2 comments</h4>
+					<div className="comment-list">
+						<Comment />
+						<Comment />
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
 
+class Comment extends React.Component {
+	render(){
+		return(
+			<div className="row mt-3 mb-3">
+				<div className="col card">
+				  <div className="card-body">
+				    <h4 className="card-title">Anne Droid</h4>
+				    <p className="card-text">
+							I wanna know what love is...
+						</p>
+				    <a href="#" className="card-link">Delete comment</a>
+				  </div>
+				</div>
+			</div>
+		);
+	}
+}
+
+
 ReactDOM.render(
-  <StoryBox />,
+  <CommentBox />,
   document.getElementById('story-app')
 );
