996d1e3777256a0fb5c6989986cf86898917a4a0
Brando Preda <brando.preda@trovaprezzi.it>
2017-09-29 17:46:04 +0200

    building comments from js list

--- Story App/component.js
+++ Story App/component.js
@@ -1,15 +1,37 @@
 class CommentBox extends React.Component {
+	_getComments(){
+		const comments = [
+			{id: 1, author: 'Brando Preda', body: 'Primo commento'},
+			{id: 2, author: 'Manuel Sproviero', body: 'Secondo commento'},
+			{id: 3, author: 'Giancarlo Giuffra', body: 'Terzo commento'},
+			{id: 4, author: 'Daniele Polli', body: 'Quarto commento'}
+		];
+
+		return comments.map((comment) => {
+			return (<Comment author={comment.author} body={comment.body} key={comment.id}/>);
+		});
+	}
+
+	_getCommentsTitle(commentCount){
+		if(commentCount === 0){
+			return 'No comments yet';
+		}
+		else if (commentCount === 1) {
+			return '1 comment';
+		}
+		return `${commentCount} comments`;
+	}
+
 	render(){
+		const comments = this._getComments();
+
 		return(
 			<div className="container">
 				<h1>Comment Box App</h1>
 				<div className="comment-box">
-					<h4 className="comment-count text-secondary">2 comments</h4>
+					<h4 className="comment-count text-secondary">{this._getCommentsTitle(comments.length)}</h4>
 					<div className="comment-list">
-						<Comment
-							author="Brando Preda" body="Primo commento" />
-						<Comment
-							author="Manuel Sproviero" body="Secondo commento" />
+						{comments}
 					</div>
 				</div>
 			</div>
